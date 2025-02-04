4d386abd3b9c2542442fb9fc551a4f58f4e4e225
Jai-Keshav-Sharma <ksharma9719@gmail.com>
2025-02-04 20:14:58 +0530

    updated

--- Trees/Questions/count_leaf_nodes.cpp
+++ Trees/Questions/count_leaf_nodes.cpp
@@ -1,9 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// Following is the Binary Tree Node class structure:
+/*************************************************************
 
-template <typename T>
+    Following is the Binary Tree node structure
+
+*************************************************************/
+template <class T>
 class BinaryTreeNode
 {
 public:
@@ -20,32 +23,51 @@ public:
 };
 
 /**
- * @brief Counts the number of leaf nodes in a binary tree.
- *
- * This function recursively traverses the binary tree and increments the count
- * whenever a leaf node is encountered. A leaf node is defined as a node with no
- * children (both left and right pointers are null).
- *
+ * @brief Modifies the given binary tree such that every node's value becomes the sum of its children's values.
+ * 
+ * This function changes the tree in a way that for every node, if the sum of its children's values is greater than 
+ * the node's value, the node's value is updated to this sum. Otherwise, the children's values are updated to the node's value.
+ * During the backtracking phase, the node's value is updated to the sum of its children's values.
+ * 
  * @param root Pointer to the root node of the binary tree.
- * @param count Reference to an integer that will store the count of leaf nodes.
  */
-void solve(BinaryTreeNode<int> *root, int &count)
+void changeTree(BinaryTreeNode<int> *root)
 {
     if (root == nullptr)
         return;
 
-    solve(root->left, count);
+    int sum = 0;
 
-    // checking for leaf node
-    if (root->left == nullptr && root->right == nullptr)
-        count++;
+    if (root->left)
+        sum += root->left->data;
+    if (root->right)
+        sum += root->right->data;
 
-    solve(root->right, count);
-}
+    // if root has value greater than sum of its children values, then assign the value of root to the children
+    if (sum > root->data)
+        root->data = sum;
+    // else assign the value of sum to the root
+    else
+    {
+        if (root->left)
+            root->left->data = root->data;
+        if (root->right)
+            root->right->data = root->data;
+    }
 
-int noOfLeafNodes(BinaryTreeNode<int> *root)
-{
-    int count = 0;
-    solve(root, count);
-    return count;
+    changeTree(root->left);
+    changeTree(root->right);
+
+    // backtracking
+    int tot = 0;
+
+    // while backtracking, replace the value of root nodes by the sum of its children
+    if (root->left)
+        tot += root->left->data;
+    if (root->right)
+        tot += root->right->data;
+
+    // replace only if current node is not a leaf node
+    if (root->left || root->right)
+        root->data = tot;
 }
